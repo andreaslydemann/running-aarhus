27ffb6283538ce980d8095847394a4ba99cce79d
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-04-14 23:14:43 +0200

    Move configureStore to utils

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { configureStore } from "./src/store";
+import { configureStore } from "utils";
 import { ThemeProvider } from "styled-components";
 import Navigator from "navigation/Navigator";
 import { Localization, AppLoading } from "expo";
