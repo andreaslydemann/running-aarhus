2b1f6379f2c559f6c7bc2d125210bfc724e5756b
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-04-28 12:26:35 +0200

    Rename runsSaga

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,8 +1,8 @@
 import { fork } from "redux-saga/effects";
 import authSaga from "./auth.saga";
-import runsSaga from "./runs.saga";
+import runSaga from "./run.saga";
 
 export default function* sagas() {
   yield fork(authSaga);
-  yield fork(runsSaga);
+  yield fork(runSaga);
 }
