97830d7ec960d1b44191464dc97fd637844fa39b
Andreas Lüdemann <andreaslydemann@gmail.com>
2019-05-26 18:17:07 +0200

    Add ActionSheet component

--- src/theme.ts
+++ src/theme.ts
@@ -14,11 +14,11 @@ export const theme = {
   info: "#E8F1F2",
   success: "#258874",
   warning: "#F77F00",
-  submitGradient: ["#258874", "#185A4D"],
+  submitGradient: ["#258874", "#185A4D"], // #1D6A5A
   danger: "#BD2F3A",
-  dangerGradient: ["#BD2F3A", "#7E2027"],
+  dangerGradient: ["#BD2F3A", "#7E2027"], // #93252D
   action: "#1481BA",
-  actionGradient: ["#1481BA", "#0F5E88"],
+  actionGradient: ["#1481BA", "#0F5E88"], // #116A99
   actionShadow: "#0e3d5b"
 };
 
